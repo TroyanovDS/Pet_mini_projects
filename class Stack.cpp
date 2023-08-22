159fe538dc26cc1fe45a253ce9b05b682dbb4705
TroyanovDS <TroyanovDS>
2023-08-22 17:06:57 +0300

    code: Рабочая версия №1

--- class Stack.cpp
+++ class Stack.cpp
@@ -3,9 +3,65 @@
 
 using namespace std;
 
+template <typename T>
+class Stack
+{
+    int n;
+    vector<T> a;
+public:
+
+Stack(T *b, int m)
+{
+    n = m;
+    
+    for (int i = 0; i < m; i++)
+    {
+        a.push_back(b[i]);
+    }
+    
+}
+
+void push (T k)
+{
+    a.push_back(k);
+}
+
+T pull ()
+{
+    T k;
+    
+    k = a.back();
+    
+    a.pop_back();
+    
+    return k;
+}
+};
+
+
+
 int main()
 {
-    cout<<"Hello World";
+    
+    int *b, n = 3;
+    
+    b = new int [n];
+    
+    for (int i = 0; i < n; i++)
+    {
+        cin >> b[i];
+    }
+    
+    
+    Stack <int> stack (b, n);
+    
+    
+    for (int i = 0; i < n; i++)
+    {
+        cout << stack.pull() << " ";
+    }
+    
+
 
     return 0;
 }
