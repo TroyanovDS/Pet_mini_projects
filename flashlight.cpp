add3108cf82fcd53b27e046f115c6d5ba8132cac
TroyanovDS <TroyanovDS>
2023-09-11 22:23:41 +0300

    code: Рабочая версия

--- flashlight.cpp
+++ flashlight.cpp
@@ -2,8 +2,60 @@
 
 using namespace std;
 
-int main ()
+float min_rad(float l, int n, float *a)
 {
+    float d = a[0];
+    
+    if (n == 1)
+    {
+        if(a[0] <= l - a[0])
+        {
+            d = l - a[0];
+        }
+    }
+    
+    else
+    {
+        if (a[0] <= l - a[n - 1])
+        {
+            d = l - a[n - 1];
+        }
+        for (int i = 1; i < n; i++)
+        {
+            if(d <= (a[i] - a[i - 1]) / 2)
+            {
+                d = ((a[i] - a[i - 1]) / 2);
+            }
+        }
+        
+        
+    }
+    
+    return d;
+}
+int main()
+{
+    float *a, l; 
+    int n;
+    
+    a = new float [n];
+    
+    cout << "length: ";
+    cin >> l;
+    
+    cout << "count of flashlights: ";
+    cin >> n;
+    
+    for (int i; i < n; i++)
+    {
+        cout << "a[" << i << "] = "; 
+        cin >> a[i];
+    }
+    
+    
+    float d = min_rad(l, n, a);
+    
+    cout << "minimal radius: " << d << endl;
 
     return 0;
 }
